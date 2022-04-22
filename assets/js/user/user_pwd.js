2a96a6496bb54195f716ae2a624e79154894f97c
dashan <18501257018@163.com>
2022-04-22 17:20:32 +0800

    个人中心完成

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,46 @@
 $(function() {
-  var form = layui.form
+        // 密码效验
+        layui.form.verify({
+                pwd: [
+                    /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+                ],
+                samePwd: function(value) {
+                    if (value === $('[name=oldpwd]').val()) {
+                        return '新密码不能和原密码一致'
+                    }
+                },
+                reqPwd: function(value) {
+                    if (value !== $('[name=newpwd]').val()) {
+                        return '两次密码不一致！'
+                    }
+                }
+            })
+            //提交
+        $('.layui-form').submit(function(e) {
+            e.preventDefault()
+            getPwd()
 
-  form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
-      if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
-      }
-    },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
-        }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
-        $('.layui-form')[0].reset()
-      }
+        })
     })
-  })
-})
+    // 请求Ajax
+function getPwd() {
+    let data = {
+        oldPwd: $('.layui-form [name=oldpwd]').val(),
+        newPwd: $('.layui-form [name=newpwd]').val()
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/my/updatepwd',
+        data: data,
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            layui.layer.msg('更改成功')
+                // 重置表单 因为JQ没有reset()  所以要先转成原生DOM$('.layui-form')[0]
+            $('.layui-form')[0].reset()
+                // 同时退出登陆
+
+        }
+    })
+}
